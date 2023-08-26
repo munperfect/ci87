b1e7cf1c35db2b03505ca405e49ce1ccf0550b9e
munperfect <maiunhi@gmail.com>
2023-08-27 01:17:00 +0700

    Initial commit

---
+++ src/pages/Products/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+import { PRODUCTS } from "../../data/products";
+
+
+export default function Product() {
+  return (
+    <div className="product-page">
+      <h1>Product page</h1>
+    </div>
+  );
+}
