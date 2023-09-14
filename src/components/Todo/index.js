0fa5871b5f60017b04421f3aa4bf90a41166efbf
munperfect <maiunhi@gmail.com>
2023-09-14 16:57:10 +0700

    Complete homework-5

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,10 +1,43 @@
-const Todo = ({ id, text, isCompleted }) => {
+import { useState } from "react";
+
+const Todo = ({ item, handleCompleteTodo, handleEditTodo }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newEditedTodo, setNewEditedTodo] = useState(item.text);
+
+  function handleToggleEditing() {
+    setIsEditing(!isEditing);
+  }
+
+  function handleSubmitEdit(e) {
+    if (e.key === "Enter" && newEditedTodo) {
+      handleEditTodo(item.id, newEditedTodo);
+      handleToggleEditing();
+    }
+  }
+
   return (
     <div>
-      <input type="checkbox" checked={isCompleted} />
-      <span style={{ textDecoration: isCompleted ? "line-through" : "none" }}>
-        {text}
-      </span>
+      <input
+        type="checkbox"
+        checked={item.isCompleted}
+        onChange={() => handleCompleteTodo(item.id)}
+      />
+
+      {isEditing ? (
+        <input
+          type="text"
+          value={newEditedTodo}
+          onChange={(e) => setNewEditedTodo(e.target.value)}
+          onKeyDown={handleSubmitEdit}
+        />
+      ) : (
+        <label
+          style={{ textDecoration: item.isCompleted ? "line-through" : "none" }}
+          onDoubleClick={handleToggleEditing}
+        >
+          {item.text}
+        </label>
+      )}
     </div>
   );
 };
